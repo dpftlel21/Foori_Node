6c88f5ebee9c94226e298aa42cf545efffef18fb
dpftlel21 <dkdnmju@naver.com>
2024-07-25 15:19:27 +0900

    feat: Google OAuth 로그인 구현

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { DB } from "./mysql.js";
 import { KakaoClient } from "./oauth/kakao.js";
 import { NaverClient } from "./oauth/naver.js";
+import { GoogleClient } from "./oauth/google.js";
 import { createToken } from "./jwt/jwt.js";
 
 
@@ -26,9 +27,20 @@ app.get("/url/kakao", (req, res, next) => {
 
 // 네이버 api 생성
 app.get("/url/naver", (req, res, next) => {
-  const url = NaverClient.getNaverAuthCodeURL();
+  const url = NaverClient.getAuthCodeURL();
 
   //console.log("naverurl", url);
+
+  res.status(200).json({
+    url,
+    });
+});
+
+// 구글 api 생성
+app.get("/url/google", (req, res, next) => {
+  const url = GoogleClient.getAuthCodeURL();
+
+  console.log("googleurl", url);
 
   res.status(200).json({
     url,
@@ -94,5 +106,36 @@ app.post('/login/naver', async (req, res) => {
       res.status(500).json({ message: 'Internal server error' });
   }
 });
+
+// 구글 로그인
+app.post('/login/google', async (req, res) => {
+  //console.log("req.body", req.body);
+  const { code } = req.body;
+  try {
+      const { access_token } = await GoogleClient.getAccessToken(code);
+      const userData = await GoogleClient.getUserData(access_token);
+      console.log("userData", userData);
+
+      //DB 사용자 등록 처리, 토큰 및 로그인 처리
+      const userCheckQuery = `SELECT * FROM MEMBER WHERE CID = ?`;
+      const userExist = await DB.query(conn, userCheckQuery, [userData.id]);
+
+      if(userExist.length === 0){
+        const insertQuery = `INSERT INTO MEMBER (CID, EMAIL, NAME, OAUTH_KIND) VALUES (?, ?, ?, ?)`;
+        await DB.query(conn, insertQuery, [userData.id, userData.email, userData.name, 'google']);
+        console.log('사용자 추가');
+      } else {
+          // 이미 존재하는 사용자
+          console.log('기존에 존재하는 사용자');
+      }
+
+
+      const token = createToken(userData);
+      res.status(200).json({ token });
+  } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
 
 export default app;
--- src/oauth/google.js
+++ src/oauth/google.js
@@ -1,26 +1,23 @@
-import crypto from 'crypto';
 
 
-const NaverClient = {
-    client_id: process.env.NAVER_CLIENT_ID,
-    client_secret: process.env.NAVER_CLIENT_SECRET,
-    redirect_uri: process.env.NAVER_REDIRECT_URI,
-    state: Math.random().toString(36),
+const GoogleClient = {
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    redirect_uri: process.env.GOOGLE_REDIRECT_URI,
     getAuthCodeURL() {
-        return `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${this.client_id}&state=${this.state}&redirect_uri=${this.redirect_uri}`;
+        return `https://accounts.google.com/o/oauth2/v2/auth?client_id=${this.client_id}&redirect_uri=${this.redirect_uri}&response_type=code&scope=email%20profile`;
     },
     async getAccessToken(code) {
         const params = {
             client_id: this.client_id,
             client_secret: this.client_secret,
-            state: this.state,
             code,
             grant_type: 'authorization_code',
             redirect_uri: this.redirect_uri,
         };
         //console.log("params", params);
         
-        const response = await fetch('https://nid.naver.com/oauth2.0/token', {
+        const response = await fetch('https://oauth2.googleapis.com/token', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
@@ -39,7 +36,7 @@ const NaverClient = {
         return tokenData;
     },
     async getUserData(token) {
-        const response = await fetch('https://openapi.naver.com/v1/nid/me', {
+        const response = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
@@ -52,4 +49,4 @@ const NaverClient = {
     }
 };
 
-export { NaverClient };
+export { GoogleClient };
